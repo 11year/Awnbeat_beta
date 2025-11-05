856f0797309d9b424c9d874ada78e676ca375a7e
11year <144410855+11year@users.noreply.github.com>
2025-11-05 23:56:18 +0000

    all-code

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#caea61',
+          secondary: '#F9C80E',
+          tertiary: '#FF4E50',
+          dark: '#0E0B16',
+          light: '#F5F5F5',
+        },
+      },
+    },
+    plugins: [],
+  };
